cd87f71b1c3d13ddb9b40947839ac2ebb4be1de2
ZijunYang <zijunhz@126.com>
2022-03-26 23:22:05 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue 	modified:   src/router/index.js 	modified:   src/views/AboutView.vue 	modified:   src/views/HomeView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-link to="/">Home Page</router-link>
+        <router-link to="/createroom">Create Room</router-link>
+        <br />
+        <router-view />
+    </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    color: rgba(255, 255, 255, 1);
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+    margin: 0;
+    padding: 0;
+    background: rgba(13, 25, 31, 1);
+}
+input {
+    background: rgba(255, 255, 255, 0.1);
+}
+button {
+    background: rgba(255, 255, 255, 0.1);
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import AboutView from '@/views/AboutView.vue'
 import HomeView from '../views/HomeView.vue'
-
+import CreateRoomView from '../views/CreateRoomView.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: AboutView
+    },
+    {
+        path: '/createroom',
+        name: 'createroom',
+        component: CreateRoomView,
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
