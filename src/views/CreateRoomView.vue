dde9510b3f290ee6e4e29e2e9b9e7ef4084b3119
ZijunYang <zijunhz@126.com>
2022-03-26 23:25:34 +0800

    new file:   src/views/CreateRoomView.vue 	new file:   src/views/JoinRoomView.vue

---
+++ src/views/CreateRoomView.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <div class="subtitle">Room ID</div>
+        <input v-on:input="checkRoomId" v-model="roomId" type="text" placeholder="Room ID" />
+        <br />
+        <div class="subtitle">Room Password</div>
+        <input v-on:input="checkRoomPsw" type="text" v-model="roomPsw" placeholder="Room Password" />
+        <br />
+        <button v-on:click="createRoom">Create Room</button>
+        <br />
+        <div>{{ info }}</div>
+    </div>
+</template>
+<script>
+import axios from "axios"
+export default {
+    name: 'CreateRoomView',
+    data() {
+        return {
+            roomId: 'sample',
+            validRoomId: 'sample',
+            roomPsw: 'sample',
+            validRoomPsw: 'sample',
+            info: '',
+        }
+    },
+    methods: {
+        createRoom() {
+            if (this.validRoomId === '' || this.validRoomPsw === '') {
+                this.info = 'Room ID and Room Password cannot be empty!'
+                return
+            }
+            this.info = ''
+            //将validRoomId和validRoomPsw发送到后端
+            //是否存在
+            //
+        },
+        checkRoomId() {
+            let flag = this.roomId.length <= 6;
+            for (let i = 0; i < this.roomId.length && flag; i++) {
+                let c = this.roomId.charAt(i)
+                if (!(('0' <= c && c <= '9') || ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))) {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                this.validRoomId = this.roomId
+                this.updateRoomInfo()
+            } else {
+                this.roomId = this.validRoomId
+            }
+        },
+        checkRoomPsw() {
+            let flag = this.roomPsw.length <= 6;
+            for (let i = 0; i < this.roomPsw.length && flag; i++) {
+                let c = this.roomPsw.charAt(i)
+                if (!(('0' <= c && c <= '9') || ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))) {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                this.validRoomPsw = this.roomPsw
+                this.updateRoomInfo()
+            } else {
+                this.roomPsw = this.validRoomPsw
+            }
+        },
+        updateRoomInfo() {
+            localStorage.setItem(`roomId`, this.validRoomId)
+            localStorage.setItem(`roomPsw`, this.validRoomPsw)
+        }
+    },
+}
+</script>
