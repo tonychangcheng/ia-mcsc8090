70e287874b245672677df8441457ae32b3ea2d3f
Zijun Yang <zijunhz@126.com>
2022-08-07 16:16:06 +0800

    add license

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
+<!--
+ Copyright (C) 2022 Zijun Yang <zijun.yang@outlook.com>
+ 
+ This file is part of God of Avalon Frontend.
+ 
+ God of Avalon Frontend is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ God of Avalon Frontend is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with God of Avalon Frontend.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
 <template>
   <div id="app">
     <div class="title">God of Avalon</div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,20 @@
+// Copyright (C) 2022 Zijun Yang <zijun.yang@outlook.com>
+// 
+// This file is part of God of Avalon Frontend.
+// 
+// God of Avalon Frontend is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// God of Avalon Frontend is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with God of Avalon Frontend.  If not, see <http://www.gnu.org/licenses/>.
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import AboutView from '@/views/AboutView.vue'
