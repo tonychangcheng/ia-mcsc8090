7b15cfdf904222dfd16b963ca7a27165bcd552de
Zijun Yang <zijunhz@126.com>
2024-07-15 22:35:41 +0800

    feat: polish background

--- src/App.vue
+++ src/App.vue
@@ -66,11 +66,11 @@ body {
       padding: 0;
       background: 
         linear-gradient(rgb(21, 30, 36), rgb(21, 30, 36)), /* 保持基本背景颜色 */
-        linear-gradient(155deg, rgba(255, 255, 255, 0.01) 25%, transparent 10%) -200px 0,
-        linear-gradient(245deg, rgba(255, 255, 255, 0.01) 25%, transparent 10%) -200px 0,
-        linear-gradient(335deg, rgba(255, 255, 255, 0.01) 25%, transparent 10%),
-        linear-gradient(65deg, rgba(255, 255, 255, 0.01) 25%, transparent 10%);
-      background-size: 400px 400px;
+        linear-gradient(155deg, rgba(255, 255, 255, 0.02) 25%, transparent 10%) -75px 0,
+        linear-gradient(245deg, rgba(255, 255, 255, 0.02) 25%, transparent 10%) -75px 0,
+        linear-gradient(335deg, rgba(255, 255, 255, 0.02) 25%, transparent 10%),
+        linear-gradient(65deg, rgba(255, 255, 255, 0.02) 25%, transparent 10%);
+      background-size: 150px 150px;
       background-blend-mode: overlay; /* 使图案与背景颜色融合 */
       background-repeat: repeat; /* 确保图案重复 */
     }
