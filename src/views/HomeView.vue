4d7eb5e8ad8855cc597e04f2a5c145c588d87fe2
Zijun Yang <zijunhz@126.com>
2024-10-27 22:10:26 -0400

    modify date

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,7 +43,7 @@
     <br>
     <div class="container">
       <div class="subtitle">更新日志</div>
-      <p><strong>2024年10月28日：</strong></p>
+      <p><strong>2024年10月27日：</strong></p>
     <ol>
         <li>修改了历史记录出现逻辑：没有记录时不显示。</li>
     </ol>
