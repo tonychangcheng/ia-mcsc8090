01ce483cc671799388acaa8a9ae55d47222abc67
Cheng Chang <tonychangcheng@gmail.com>
2025-09-08 20:33:55 +1000

    add backend url

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'; // Use createStore for Vuex 4
+
+export default createStore({
+  state: {
+    // change 'server' to your backend without '/' at the end.
+    server: 'ia-api-qmw9.onrender.com',
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+});
