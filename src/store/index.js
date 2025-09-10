690fc01554113ae895df8fd557c5d5cc5f7adf70
Cheng Chang <tonychangcheng@gmail.com>
2025-09-10 16:28:17 +1000

    fix server uil

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { createStore } from 'vuex'; // Use createStore for Vuex 4
 export default createStore({
   state: {
     // change 'server' to your backend without '/' at the end.
-    server: 'https//ia-api-qmw9.onrender.com',
+    server: 'https://ia-api-qmw9.onrender.com',
   },
   getters: {
   },
